30d23296bfb02978fc641ff97f0d19750d3a31b9
Jonny Saykosy <josayko@pm.me>
2023-08-20 14:03:43 +0200

    1:37:57: RSS Feed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	import Header from './header.svelte';
 	import Footer from './footer.svelte';
+	import PageTransition from './transition.svelte';
 
 	import 'open-props/style';
 	import 'open-props/normalize';
 	import 'open-props/buttons';
 
 	import '../app.css';
+
+	export let data;
 </script>
 
 <div class="layout">
 	<Header />
 	<main>
-		<slot />
+		<PageTransition url={data.url}>
+			<slot />
+		</PageTransition>
 	</main>
 	<Footer />
 </div>
